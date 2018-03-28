c2513ba4aea1ad885199aa8fa3bfeff86a5167ed
Joseph Marrero <marrero250@gmail.com>
2018-03-28 10:23:37 -0400

    fragments info, pagos, solicitudes

--- app/src/main/java/com/example/josephmarrero/cantv/Fragments/SolicitudesFragment.java
+++ app/src/main/java/com/example/josephmarrero/cantv/Fragments/SolicitudesFragment.java
@@ -21,6 +21,8 @@ public class SolicitudesFragment extends Fragment {
 
     String planesList[] = {"2Mbps", "3Mbps", "6Mbps", "10Mbps"};
     View view;
+private boolean isFirsTime = true;
+    private boolean isFirsTime1 = true;
 
     public SolicitudesFragment() {
         // Required empty public constructor
@@ -36,17 +38,57 @@ public class SolicitudesFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_solicitudes,container,false);
         // Initialize the spinner
         Spinner spSimpleSpinner = (Spinner) view.findViewById(R.id.spinner);
-        // Adapter will get the values from array and then show in the spinner
-        ArrayAdapter adapter = new ArrayAdapter(getActivity(),android.R.layout.simple_list_item_1,planesList);
-        spSimpleSpinner.setAdapter(adapter);
 
-        spSimpleSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+       // Spinner spSimpleSpinner1 = (Spinner) view.findViewById(R.id.spinnerPlanesLocales);
+
+        Spinner spinnerListPlan = (Spinner) view.findViewById(R.id.spinnerPlanesLocales);
+
+
+        // Adapter will get the values from array and then show in the spinner
+       //ArrayAdapter adapterList = new ArrayAdapter(getActivity(),android.R.layout.simple_list_item_1,planesLocales);
+       // spSimpleSpinner1.setAdapter(adapterList);
+
+     //  ArrayAdapter adapter = new ArrayAdapter(getActivity(),android.R.layout.simple_list_item_1,planesList);
+      // spSimpleSpinner.setAdapter(adapter);
+
+       ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getContext(), R.array.Planes_locales, android.R.layout.simple_spinner_item);
+       ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this.getContext(),R.array.Planes_Aba, android.R.layout.simple_spinner_item);
+
+       adapter1.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
+       spSimpleSpinner.setAdapter(adapter1);
+       adapter.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
+        spinnerListPlan.setAdapter(adapter);
+
+        spinnerListPlan.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+
+                if (isFirsTime1){
+                    isFirsTime1 = false;
+                }else {
+                    Toast.makeText(adapterView.getContext(), "Su plan a sido cambiado con exito a " + (String) adapterView.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+      spSimpleSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id)
             {
-                Toast.makeText(adapterView.getContext(), "Su plan a sido cambiado con exito a " + (String) adapterView.getItemAtPosition(pos), Toast.LENGTH_SHORT).show();
 
+                if (isFirsTime){
+                    isFirsTime = false;
+                }else {
+                    Toast.makeText(adapterView.getContext(), "Su plan a sido cambiado con exito a " + (String) adapterView.getItemAtPosition(pos), Toast.LENGTH_SHORT).show();
+                }
 
             }
 
@@ -58,6 +100,9 @@ public class SolicitudesFragment extends Fragment {
         return view;
 
     }
+
+
+
 
 
     @Override
--- app/src/main/java/com/example/josephmarrero/cantv/Fragments/SoporteTecFragment.java
+++ app/src/main/java/com/example/josephmarrero/cantv/Fragments/SoporteTecFragment.java
@@ -6,6 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.josephmarrero.cantv.R;
 
@@ -14,6 +18,8 @@ import com.example.josephmarrero.cantv.R;
  */
 public class SoporteTecFragment extends Fragment {
 
+    private boolean isFirsTime2 = true;
+    View view;
 
     public SoporteTecFragment() {
         // Required empty public constructor
@@ -24,7 +30,37 @@ public class SoporteTecFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_soporte_tec, container, false);
+       // return inflater.inflate(R.layout.fragment_soporte_tec, container, false);
+        View view = inflater.inflate(R.layout.fragment_soporte_tec,container,false);
+
+        Spinner spinnerAverias = (Spinner) view.findViewById(R.id.spinnerReporteAverias);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getContext(), R.array.
+                Reporte_Averias, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
+        spinnerAverias.setAdapter(adapter);
+
+        spinnerAverias.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+
+                if (isFirsTime2){
+                    isFirsTime2 = false;
+                }else {
+                    Toast.makeText(adapterView.getContext(), "El promedio de respuesta de la averia " + (String) adapterView.getItemAtPosition(position) + " Podria ser de 48 Horas habiles", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        return view;
     }
+
 
 }
